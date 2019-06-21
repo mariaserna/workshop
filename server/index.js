d3bbfda26b60b936133046d4e37960f2ecd25874
Maria <maria.antonia@jet.com>
2019-06-21 16:04:04 -0400

    graphql server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const next = require('next');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -10,7 +12,13 @@ const PORT = process.env.PORT || 3000;
 
 app.prepare()
   .then(() => {
-    server.get('/test', (req, res) => res.send('test'));
+    server.use('/graphql', graphqlHTTP({ // use : get and post
+      schema,
+      graphiql: true,
+    }));
+
+
+    // server.get('/test', (req, res) => res.send('test'));
 
     server.get('*', (req, res) => handle(req, res));
 
