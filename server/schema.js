575b0c51a319f03105d3758a442f5257556536a6
Maria <maria.antonia@jet.com>
2019-06-21 16:12:05 -0400

    update users

--- server/schema.js
+++ server/schema.js
@@ -6,9 +6,17 @@ const schema = makeExecutableSchema({
       id: Int!
       title: String
     }
+
+    type User {
+        id: Int
+        name: String!
+        lastname: String
+        email: String!
+    }
  
     type Query {
       posts: [Post]
+      users: [User]
     }
   `,
   resolvers: {
@@ -16,6 +24,24 @@ const schema = makeExecutableSchema({
       posts: () => [{
         id: 1,
         title: 'Mi titulo',
+      }],
+      users: () => [{
+        id: 123,
+        name: 'Antonia',
+        lastname: 'Serna',
+        email: 'anto@zemoga.com',
+      },
+      {
+        id: 456,
+        name: 'Orlando',
+        lastname: 'Donado',
+        email: 'orli@zemoga.com',
+      },
+      {
+        id: 678,
+        name: 'Hello',
+        lastname: 'Lastname',
+        email: 'anto@zemoga.com',
       }],
     },
   },
