42444d8f180e0e39fe0031e979ed27696307e398
Maria <maria.antonia@jet.com>
2019-06-26 11:33:16 -0400

    update config

--- apollo/client.js
+++ apollo/client.js
@@ -1,10 +1,13 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
+import getConfig from 'next/config';
 
-const HOST = process.env.HOST || 'http://localhost:3000';
+const { publicRuntimeConfig } = getConfig();
+
+// const HOST = process.env.HOST || 'http://localhost:3000';
 const client = new ApolloClient({
   fetch,
-  uri: `${HOST}/graphql`,
+  uri: `${publicRuntimeConfig.HOST}/graphql`,
 });
 
 export default client;
