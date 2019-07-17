174b039fdb3b058ff967dfb495df46b7d43cee50
Maria <maria.antonia@jet.com>
2019-07-17 16:42:13 -0400

    remove code

--- pages/users.js
+++ pages/users.js
@@ -45,33 +45,3 @@ const Users = () => {
 };
 
 export default Users;
-
-
-// import React from 'react';
-// import {
-//   Box,
-//   Paper,
-//   Container,
-// } from '@material-ui/core';
-// import Posts from '../components/Posts';
-
-// // const handleClick = () => {
-// //   console.log('Hola!!');
-// // };
-
-// const Home = () => (
-//   <Container spacing={2}>
-//     <Paper>
-//       <Box m={3}>
-//         <h1>Anto!!</h1>
-//         {/* <Button variant="contained" color="primary" onClick={handleClick}>
-//           Button
-//         </Button> */}
-//       </Box>
-//     </Paper>
-//     <Posts />
-//   </Container>
-// );
-
-
-// export default Home;
