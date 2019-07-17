8e89143e7539a1142c3a55c9ea5f70f84abceb0b
Maria <maria.antonia@jet.com>
2019-07-17 17:28:30 -0400

    server side

--- pages/users.js
+++ pages/users.js
@@ -6,50 +6,90 @@ import {
   Card,
   CardContent,
 } from '@material-ui/core';
-import useQuery from '../components/hooks/useQuery';
+import useQuery, { runQuery } from '../components/hooks/useQuery';
 import usersQuery from '../components/graphql/usersQuery';
-import Loading from '../components/Loading';
+
+// const Users = () => {
+//   const {
+//     data,
+//     loading,
+//     error,
+//   } = useQuery(usersQuery);
+//   const { users } = data;
+
+//   if (error) {
+//     return null;
+//   }
+//   return (
+//     <Container maxWidth="sm">
+//       { loading
+//         ? <Loading />
+//         : users.map(user => (
+//           <Card>
+//             <CardContent>
+//               <Typography variant="h4">{user.name}</Typography>
+//               <div>{`<${user.email}>`}</div>
+//               { !!user.projects.length && (
+//                 <>
+//                   <Typography variant="h5">Projects</Typography>
+//                   <div>
+//                     {user.projects.map(project => (
+//                       <>
+//                         <div>{project.name}</div>
+//                         <Button variant="contained" color="primary">Open Proyect</Button>
+//                       </>
+//                     ))}
+//                   </div>
+//                 </>
+//               )}
+//             </CardContent>
+//           </Card>
+//         ))
+//       }
+//     </Container>
+//   );
+// };
 
 
-const Users = () => {
+const Users = ({
+  users,
+}) => (
+  <Container maxWidth="sm">
+    {users.map(user => (
+      <Card>
+        <CardContent>
+          <Typography variant="h4">{user.name}</Typography>
+          <div>{`<${user.email}>`}</div>
+          { !!user.projects.length && (
+            <>
+              <Typography variant="h5">Projects</Typography>
+              <div>
+                {user.projects.map(project => (
+                  <>
+                    <div>{project.name}</div>
+                    <Button variant="contained" color="primary">Open Proyect</Button>
+                  </>
+                ))}
+              </div>
+            </>
+          )}
+        </CardContent>
+      </Card>    
+    ))}
+  </Container>
+);
+
+
+Users.getInitialProps = async () => {
   const {
     data,
-    loading,
     error,
-  } = useQuery(usersQuery);
-  const { users } = data;
+  } = await runQuery(usersQuery);
 
-  if (error) {
-    return null;
-  }
-  return (
-    <Container maxWidth="sm">
-      { loading
-        ? <Loading />
-        : users.map(user => (
-          <Card>
-            <CardContent>
-              <Typography variant="h4">{user.name}</Typography>
-              <div>{`<${user.email}>`}</div>
-              { !!user.projects.length && (
-                <>
-                  <Typography variant="h5">Projects</Typography>
-                  <div>
-                    {user.projects.map(project => (
-                      <>
-                        <div>{project.name}</div>
-                        <Button variant="contained" color="primary">Open Proyect</Button>
-                      </>
-                    ))}
-                  </div>
-                </>
-              )}
-            </CardContent>
-          </Card>
-        ))
-      }
-    </Container>
-  );
+  return {
+    users: data.users || [],
+    error,
+  };
 };
 
 export default Users;
